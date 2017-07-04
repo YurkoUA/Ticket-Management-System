fb6439ad1c90505725b70c404a579de2c61f75ed
Yurii Nosach <nosach.yurko@gmail.com>
2017-07-04 17:53:20 +0300

    Fixed problem with timezones #2

--- TicketManagementSystem.Web/Helpers/LocalDateHelper.cs
+++ TicketManagementSystem.Web/Helpers/LocalDateHelper.cs
@@ -5,11 +5,11 @@ namespace TicketManagementSystem.Web
 {
     public static class LocalDateHelper
     {
-        public static MvcHtmlString DisplayLocalDate(this HtmlHelper html, DateTime dateTime, string format)
+        public static MvcHtmlString DisplayDateTime(this HtmlHelper html, DateTime dateTime, string format)
         {
             var span = new TagBuilder("span");
-            span.SetInnerText(dateTime.ToLocalTime().ToString(format));
-
+            span.MergeAttribute("data-date", (dateTime.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds).ToString());
+            span.SetInnerText(dateTime.ToString(format));
             return MvcHtmlString.Create(span.ToString());
         }
     }
--- TicketManagementSystem.Web/Scripts/site.js
+++ TicketManagementSystem.Web/Scripts/site.js
@@ -1,4 +1,14 @@
-﻿function showModal(data) {
+﻿convertDate();
+
+function showModal(data) {
     $('#modal-window-content').html(data);
     $('#modal-window').modal('show');
+    convertDate();
+}
+
+function convertDate() {
+    $('[data-date]').each(function () {
+        var localDate = new Date(parseInt($(this).attr('data-date')));
+        $(this).html(localDate.toLocaleString());
+    });
 }
