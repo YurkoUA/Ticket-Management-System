4980dc2d85cd00d127d382032a9821d751a870e2
Yurii Nosach <nosach.yurko@gmail.com>
2017-08-22 14:29:20 +0300

    Added "Check All" button to MoveUnallocatedPartial.cshtml

--- TicketManagementSystem.Web/Scripts/package.js
+++ TicketManagementSystem.Web/Scripts/package.js
@@ -6,3 +6,10 @@
 function OnSuccess(data) {
     $('#package-toolbar').html('');
 }
+
+// Function to check all checkboxes in "MoveUnallocatedPartial".
+function checkAll() {
+    $("#move-unallocated-grid input[type='checkbox']").each(function (index, element) {
+        element.setAttribute("checked", "true");
+    });
+}
