e724c717a9121ab0b2902ad4be29a34fb354f1f3
Yurii Nosach <nosach.yurko@gmail.com>
2017-08-25 13:50:34 +0300

    Ticket/Create -CreateInPackage: added clipboard (in dropdown) for Note. // Updated Bootstrap to 3.3.7

--- TicketManagementSystem.Web/Scripts/ticket-create.js
+++ TicketManagementSystem.Web/Scripts/ticket-create.js
@@ -1,12 +1,48 @@
-﻿function OnBegin() {
+﻿var notesArray = [];
+
+$("#note-dropdown").on('click', 'li', function () {
+    var text = $(this).attr('text-to-append');
+
+    console.log('to append: ' + text);
+
+    if (text !== undefined) {
+        $('#Note').val(text);
+    }
+});
+
+function OnBegin() {
     $('#create-result').html("");
 }
 
 function OnSuccess(data) {
+    pushNoteToArray($("#Note").val());
+
     $('#Number').val('');
     $('#SerialNumber').val('');
     $('#Note').val('');
     $('#Date').val('');
 
     $('#Number').focus();
+}
+
+function pushNoteToArray(note) {
+    if (note !== undefined && notesArray.indexOf(note) < 0) {
+        notesArray.push(note);
+        appendToDropdown(note);
+
+        console.log('pushed: ' + note);
+    }
+}
+
+function appendToDropdown(note) {
+    $('#note-dropdown').prepend('<li text-to-append="' + note + '"><a>' + getShortNoteText(note) + '</a></li>');
+}
+
+function getShortNoteText(note) {
+    var maxLength = 20;
+
+    if (note.length > maxLength)
+        note = note.substr(0, maxLength) + "...";
+
+    return note;
 }
